bc10f028be3f018b85d79441208faf1872cac149
ram <rramkumar1306@gmail.com>
2024-04-23 22:17:49 +0530

    Initialized.

---
+++ Source/NeedMoney/NeedMoneyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NeedMoneyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANeedMoneyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANeedMoneyGameMode();
+};
+
+
+
